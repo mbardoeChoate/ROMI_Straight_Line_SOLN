50dd923dc485f721ab1d4958126ceafc84fae8f1
Matthew Bardoe <mbardoe@gmail.com>
2023-04-14 01:19:31 -0400

    Added in DriveStraight

---
+++ drivestraight.py
@@ -0,0 +1,18 @@
+
+class DriveStraight:
+
+    def __init__(self, drivetrain, goal_in_meters):
+        self.drivetrain=drivetrain
+        self.goal=goal_in_meters
+        self.kp=-20
+
+    def run(self):
+
+        difference=self.drivetrain.getLeftDistanceMeter()-self.drivetrain.getRightDistanceMeter()
+        if self.drivetrain.averageDistanceMeter() > self.goal:
+            self.drivetrain.arcadeDrive(0,0)
+        else:
+            rotate=difference*self.kp
+            forward=.4
+            print(f"Fwd: {forward}, Rot: {rotate}  distance:{self.drivetrain.averageDistanceMeter()} difference:{difference}")
+            self.drivetrain.arcadeDrive(rotate, forward)
