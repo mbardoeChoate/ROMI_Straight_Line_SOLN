b01b81f9676f36876494633e3be50117523ca696
Matthew Bardoe <mbardoe@gmail.com>
2023-04-26 00:54:43 -0400

    Created Protocol for an AutoRoutine

--- robotcontainer.py
+++ robotcontainer.py
@@ -1,15 +1,18 @@
 import wpilib
-import typing
+
+from autoroutine import AutoRoutine
 from drivestraight import DriveStraight
-from gyroturn import GyroTurn
 from drivetrain import Drivetrain
+from gyroturn import GyroTurn
+
+
 class RobotContainer:
 
-    def __init__(self)->None:
-        self.controller=wpilib.Joystick(0)
+    def __init__(self) -> None:
+        self.controller = wpilib.Joystick(0)
         # Create SmartDashboard chooser for autonomous routines
-        self.chooser=wpilib.SendableChooser()
-        self.drivetrain=Drivetrain()
+        self.chooser = wpilib.SendableChooser()
+        self.drivetrain = Drivetrain()
         self._configure()
 
     def _configure(self):
@@ -17,5 +20,5 @@ class RobotContainer:
         self.chooser.addOption("Go straight 2m", DriveStraight(self.drivetrain, 2))
         wpilib.SmartDashboard.putData(self.chooser)
 
-    def get_autonomous(self):
+    def get_autonomous(self) -> AutoRoutine:
         return self.chooser.getSelected()
