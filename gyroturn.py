e21ded201d9ccc53b659e834b820ce218976294c
Matthew Bardoe <mbardoe@gmail.com>
2023-04-29 15:29:19 -0400

    Used the wpilib PID controller to do the same thing as my personal PID controller.

--- gyroturn.py
+++ gyroturn.py
@@ -1,26 +1,41 @@
 from drivetrain import Drivetrain
-
+from wpimath.controller import PIDController
 
 class GyroTurn:
 
     def __init__(self, drivetrain: Drivetrain, angle: float):
         self.angle = angle
         self.drivetrain = drivetrain
-        self.kp = 1 / 25
-        self.ki = 1 / 800
+        self.pid_controller=PIDController(1 / 60, 1/20, 1/650 )
+        self.pid_controller.setIntegratorRange(-.1, .1)
+        self.pid_controller.setSetpoint(self.angle)
+        self.pid_controller.setTolerance(1)
+        #self.kp = 1 / 25
+        #self.ki = 1 / 800
         self.drivetrain.resetGyro()
-        self.total_error = 0
+        #self.total_error = 0
 
     def run(self):
         current_reading = self.drivetrain.getGyroAngleZ()
-        error = self.angle - current_reading
-        self.total_error += error
-        self.total_error = min(500, self.total_error)
-        # print(f"gyro: {current_reading} push: {self.kp * error}")
-        if abs(error) > 1:
-            power = min(.4, max(-.4, self.kp * error + self.ki * self.total_error))
-            print(f"gyro: {current_reading} push: {power}")
-            self.drivetrain.arcadeDrive(power, 0)
+        power=self.pid_controller.calculate(current_reading)
+        atSetpoint=self.pid_controller.atSetpoint()
+        position_error=self.pid_controller.getPositionError()
+        velocity_error=self.pid_controller.getVelocityError()
+        print(f"{current_reading=}  {power=} {atSetpoint=} {position_error=} {velocity_error=}" )
+        if not atSetpoint:
+            self.drivetrain.arcadeDrive(power,0)
         else:
-            self.drivetrain.arcadeDrive(0, 0)
-            self.total_error = 0
+            self.drivetrain.arcadeDrive(0,0)
+
+        # error = self.angle - current_reading
+        # self.total_error += error
+        # self.total_error = min(500, self.total_error)
+        # # print(f"gyro: {current_reading} push: {self.kp * error}")
+        # if abs(error) > 1:
+        #     power = min(.4, max(-.4, self.kp * error + self.ki * self.total_error))
+        #     print(f"gyro: {current_reading} push: {power}")
+        #     self.drivetrain.arcadeDrive(power, 0)
+        # else:
+        #     self.drivetrain.arcadeDrive(0, 0)
+        #     self.total_error = 0
+
