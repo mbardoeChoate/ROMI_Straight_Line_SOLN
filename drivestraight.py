5f697c2dd7520268c4cd41c7110dc90ea687982f
Matthew Bardoe <mbardoe@gmail.com>
2023-05-01 16:22:18 -0400

    Attempt 1. Untested code.

--- drivestraight.py
+++ drivestraight.py
@@ -1,5 +1,6 @@
-from autoroutine import AutoRoutine
 from wpimath.controller import PIDController
+
+from autoroutine import AutoRoutine
 
 
 class DriveStraight(AutoRoutine):
@@ -7,24 +8,25 @@ class DriveStraight(AutoRoutine):
     def __init__(self, drivetrain, goal_in_meters):
         self.drivetrain = drivetrain
         self.goal = goal_in_meters
-        self.pid_controller=PIDController(20, 0, 0)
-        self.pid_controller.setSetpoint(0)
-        self.pid_controller.setTolerance(.05)
+        self.pid_controller_direc = PIDController(20, 0, 0)
+        self.pid_controller_direc.setSetpoint(0)
+        self.pid_controller_direc.setTolerance(.05)
+        self.pid_controller_dist = PIDController(5 / (2 * goal_in_meters), 0, 0)
+        self.pid_controller_dist.setSetpoint(goal_in_meters)
+        self.pid_controller_dist.setTolerance(.05)
 
-        #self.kp = -20
+
 
     def run(self):
 
         difference = self.drivetrain.getLeftDistanceMeter() - self.drivetrain.getRightDistanceMeter()
-        rotate=self.pid_controller.calculate(difference)
-        if self.pid_controller.atSetpoint():
-            rotate=0
-        if self.drivetrain.averageDistanceMeter() > self.goal:
+        rotate = self.pid_controller_direc.calculate(difference)
+        distance = self.drivetrain.averageDistanceMeter()
+        if self.pid_controller_direc.atSetpoint():
+            rotate = 0
+        forward = self.pid_controller_dist.calculate(distance)
+        if self.pid_controller_dist.atSetpoint():
             self.drivetrain.arcadeDrive(0, 0)
         else:
-            #rotate = difference * self.kp
-            # rotate=0
-            forward = .4
-            print(
-                f"Fwd: {forward}, Rot: {rotate}  distance:{self.drivetrain.averageDistanceMeter()} difference:{difference}")
+            print(f"{forward=} {rotate=} {distance=} {difference=}")
             self.drivetrain.arcadeDrive(rotate, forward)
