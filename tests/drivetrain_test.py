6f82d4f9fcd807562448ea052dcc4271d2b5abab
Matthew Bardoe <mbardoe@gmail.com>
2023-05-12 07:49:04 -0400

    Some small drivetrain edits

--- tests/drivetrain_test.py
+++ tests/drivetrain_test.py
@@ -21,8 +21,10 @@ def drivetrain() -> Drivetrain:
 
 
 def test_reset_encoders(drivetrain: Drivetrain):
+    '''A test to make sure that when we reset the encoders all the encoders are reset.
+    '''
     # Setup
-    # Need acces to the reset functions of each encoder
+    # Need access to the reset functions of each encoder
     left_reset = drivetrain.leftEncoder.reset
     right_reset = drivetrain.rightEncoder.reset
 
@@ -34,13 +36,19 @@ def test_reset_encoders(drivetrain: Drivetrain):
     right_reset.assert_called_once()
 
 
-def test_averageDistanceMeter(drivetrain: Drivetrain, monkeypatch: MonkeyPatch) -> None:
+@pytest.mark.parametrize(('left_Distance', 'right_Distance', 'output'), (
+        (2, 3, 2.5),
+        (10, 20, 15),
+        (-3, 3, 0)
+), )
+def test_averageDistanceMeter(drivetrain: Drivetrain, monkeypatch: MonkeyPatch, left_Distance, right_Distance,
+                              output) -> None:
     # Setup
     def mock_getRightDistanceMeter(self):
-        return 3.0
+        return left_Distance
 
     def mock_getLeftDistanceMeter(self):
-        return 2.0
+        return right_Distance
 
     monkeypatch.setattr(Drivetrain, "getLeftDistanceMeter", mock_getRightDistanceMeter)
     monkeypatch.setattr(Drivetrain, "getRightDistanceMeter", mock_getLeftDistanceMeter)
@@ -49,7 +57,7 @@ def test_averageDistanceMeter(drivetrain: Drivetrain, monkeypatch: MonkeyPatch) 
     dist = drivetrain.averageDistanceMeter()
 
     # Assert
-    assert dist == 2.5
+    assert dist == output
 
 
 def test_arcadeDrive(drivetrain: Drivetrain) -> None:
